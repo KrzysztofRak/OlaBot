0ca1b76ca442b3ab9fcb6941fee1bc4c59ac675c
krzysztof.rak.1995@gmail.com <krzysztof.rak.1995@gmail.com>
2018-07-27 10:24:35 +0200

    Completed logging to chomikuj service.

---
+++ Chomikuj.API/Extensions/CookieWebClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Chomikuj.API.Extensions
+{
+    public class WebClientWithCookies : WebClient
+    {
+        public CookieContainer CookieContainer { get; private set; }
+
+        /// <summary>
+        /// This will instanciate an internal CookieContainer.
+        /// </summary>
+        public WebClientWithCookies()
+        {
+            this.CookieContainer = new CookieContainer();
+        }
+
+        /// <summary>
+        /// Use this if you want to control the CookieContainer outside this class.
+        /// </summary>
+        public WebClientWithCookies(CookieContainer cookieContainer)
+        {
+            this.CookieContainer = cookieContainer;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var request = base.GetWebRequest(address) as HttpWebRequest;
+            if (request == null) return base.GetWebRequest(address);
+            request.CookieContainer = CookieContainer;
+            return request;
+        }
+    }
+}
---
+++ Test.APP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Chomikuj.API;
+
+namespace Test.APP
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChomikujConnection conn = new ChomikujConnection("GameOverMan", "5991DSAwyipmbczK");
+        }
+    }
+}
